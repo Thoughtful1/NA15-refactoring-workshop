24b4e1a528ad7feb43e1d9a183dd8f491cf8e9b3
Thoughtful1 <63436875+Thoughtful1@users.noreply.github.com>
2021-09-02 20:52:22 +0200

    some tests are passing

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -212,6 +212,10 @@ Controller::Segment Controller::getNewHead() const
 
     return newHead;
 }
+void Controller::handlePauseOfGame(const PauseInd& pausedGame) 
+{
+  
+}
 
 void Controller::receive(std::unique_ptr<Event> e)
 {
@@ -221,6 +225,7 @@ void Controller::receive(std::unique_ptr<Event> e)
         case DirectionInd::MESSAGE_ID: return handleDirectionChange(*static_cast<EventT<DirectionInd> const&>(*e));
         case FoodInd::MESSAGE_ID: return handleFoodPositionChange(*static_cast<EventT<FoodInd> const&>(*e));
         case FoodResp::MESSAGE_ID: return handleNewFood(*static_cast<EventT<FoodResp> const&>(*e));
+        case PauseInd::MESSAGE_ID: return handlePauseOfGame(*static_cast<EventT<PauseInd> const&>(*e));
         default: throw UnexpectedEventException();
     };
 }
